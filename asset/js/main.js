f80b7576d72da80b96e018b1f56a4555a2ab423e
emanmoq <42995029+emanmoq@users.noreply.github.com>
2019-05-17 13:16:40 -0700

    dddd

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+    var wow = new WOW(
+        {
+          boxClass:     'wow',   
+          mobile:false,
+
+          // animated element css class (default is wow)
+          animateClass: 'animated', // animation css class (default is animated)
+          offset:       0,          // distance to the element when triggering the animation (default is 0)
+          live:         true,       // act on asynchronously loaded content (default is true)
+          callback:     function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+          },
+          scrollContainer: null // optional scroll container selector, otherwise use window
+        }
+      );
+      wow.init();
+
     shareItem=false;
     $(".shareItemDisplay").click(function() {
         if(shareItem)
@@ -9,7 +27,79 @@ $(document).ready(function () {
         }
         shareItem=!shareItem;
       });
+      $(".nav-link").click(function() {
+       $(".nav-link").removeClass("aciveLink");
+      $(this).addClass("aciveLink");
+      });
+      $(window).scroll(function () {
+        scrollTop = $(window).scrollTop();
+
+         if( scrollTop >0){
+            $('.navbar').addClass('scrollNav');
+
+         }
+         else{
+            $('.navbar').removeClass('scrollNav');
+         }
+         scrollTop = $(window).scrollTop()
+
+         if(scrollTop>=$("#services").offset().top & scrollTop < $(".AcceptedSec").offset().top ){
+            $(".nav-link").removeClass("activeLink");
+            $(".servicesLink").addClass("activeLink");
+         }
+         else if(scrollTop>=$("#team").offset().top & scrollTop < $(".makeAppoint").offset().top ){
+            $(".nav-link").removeClass("activeLink");
+            $(".teamLink").addClass("activeLink");
+           
+         }
+         else if(scrollTop>=$("#blog").offset().top & scrollTop < $(".GuestSec").offset().top ){
+            $(".nav-link").removeClass("activeLink");
+            $(".blogLink").addClass("activeLink");
+           
+         }
+         else if(scrollTop>=$("#contact").offset().top ){
+            $(".nav-link").removeClass("activeLink");
+            $(".contactLink").addClass("activeLink");
+           
+         }
    
+        });
+        $(".servicesLink").click(function() { 
+	
+            $('.nav-link').removeClass('activeLink');
+            $(this).addClass('activeLink');
+            
+                $('html, body').animate({
+                    scrollTop: $("#services").offset().top
+                }, 1000);
+            });
+            $(".teamLink").click(function() { 
+	
+                $('.nav-link').removeClass('activeLink');
+                $(this).addClass('activeLink');
+                
+                    $('html, body').animate({
+                        scrollTop: $("#team").offset().top
+                    }, 1000);
+                });
+                $(".blogLink").click(function() { 
+	
+                    $('.nav-link').removeClass('activeLink');
+                    $(this).addClass('activeLink');
+                    
+                        $('html, body').animate({
+                            scrollTop: $("#blog").offset().top
+                        }, 1000);
+                    });
+                    $(".contactLink").click(function() { 
+	
+                        $('.nav-link').removeClass('activeLink');
+                        $(this).addClass('activeLink');
+                        
+                            $('html, body').animate({
+                                scrollTop: $("#contact").offset().top
+                            }, 1000);
+                        });
 
       $('.DoctorSlider').owlCarousel({
         loop:true,
@@ -25,6 +115,19 @@ $(document).ready(function () {
             },
             1000:{
                 items:4
+            }
+        }
+    });
+    $('.GuestCarusel').owlCarousel({
+        navText: ["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"],
+        loop:true,
+        autoplay:false,
+        nav:true,
+        margin:20,
+        responsive:{
+            0:{
+                items:1
+         
             }
         }
     });
